272f6e6bfce2c89f7d14b718e20a2940dfb14536
Aditya-xrpl3 <aditkazindar@gmail.com>
2025-09-13 08:34:39 +0700

    error

--- gudang/admin.py
+++ gudang/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import Barang, UserProfile
+
+@admin.register(Barang)
+class BarangAdmin(admin.ModelAdmin):
+    list_display = ['nama', 'tempat_produksi', 'halal', 'tanggal_kadaluarsa', 'kadaluarsa']
+    list_filter = ['halal', 'tanggal_kadaluarsa']
+    search_fields = ['nama', 'tempat_produksi']
+    
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'User Profile' 
+    
+class CustomUserAdmin(UserAdmin):
+    inlines = (UserProfileInline, )
+    
+admin.site.unregister(User)
+admin.site.register(User,CustomUserAdmin)
 
 # Register your models here.
